715c39a41f463154cf7f98273f21232c5565a870
LeoG <goubardleo@gmail.com>
2020-11-20 16:50:45 +0100

    We just got the right fetch for the feed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  const [subreddit, setSubreddit] = useState('webdev');
+
+  useEffect(()=>{
+    fetch("https://www.reddit.com/r/webdev.json").then(res =>{
+      if (res.status !== 200) {
+        console.log("ERROR");
+        return;
+      }
+
+      res.json().then(data => {
+        if(data != null) {
+          console.log(data);
+        }
+      });
+    })
+  }, [subreddit]);
+  
   return (
     <div className="App">
       <header className="App-header">
